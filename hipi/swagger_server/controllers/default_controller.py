cc6400350ce6625524396b35de2b7fa9455a63b6
Diego León <diegoleon@MacBook-Air-de-Diego.local>
2024-09-05 07:54:31 -0400

    Actualización de controladores

--- hipi/swagger_server/controllers/default_controller.py
+++ hipi/swagger_server/controllers/default_controller.py
@@ -12,6 +12,7 @@ import pandas as pd
 import os
 import time
 from delight import *
+import subprocess
 
 import cProfile, pstats, io
 
@@ -38,30 +39,33 @@ def get_files(dataframe, chunk_index):
     """Worker function to process chunks of the dataframe and write to a temporary ZIP file."""
     B = {}
     temp_zip_path = f"/tmp/temp_zip_{chunk_index}.zip"
+    inicio=time.time()
     with zipfile.ZipFile(temp_zip_path, 'w', zipfile.ZIP_DEFLATED) as zip_file:
         for idx, oid in dataframe.iterrows():
-            
             A = []
             ra, dec, oid_str = oid['ra'], oid['dec'], str(oid['oid'])
             
             for k in range(5):
                 # Replace this with your actual method to get FITS cutout
-                fits_cutout = tesis_opt.fits_cutout(ra, dec, 11 - k, 30)
-                A.append([fits_cutout, ra, dec, 11 - k, oid_str])
+                try:
+                    A.append([tesis_opt.fits_cutout(ra, dec, 11 - k, 30), ra, dec, 11 - k, oid_str])
+                except(FileNotFoundError):
+                    print("WARNING: " + str(ra) +"," + str(dec))
             
             B[oid_str] = A
-            
-            for folder, files in B.items():
-                folder_path = os.path.join('multiresolution_fits_files', folder)
-                zip_file.writestr(folder_path + '/', '')
+        print(time.time()-inicio)    
+        for folder, files in B.items():
+            folder_path = os.path.join('multiresolution_fits_files', folder)
+            zip_file.writestr(folder_path + '/', '')
 
-                for filename in files:
-                    hdulist = filename[0]
-                    with io.BytesIO() as fits_binary_data:
-                        hdulist.writeto(fits_binary_data, checksum=True, overwrite=True)
-                        fits_binary_data.seek(0)
-                        zip_file.writestr(os.path.join(folder_path, f"{filename[4]}_order{filename[3]}.fits"), fits_binary_data.getvalue())
-                    hdulist.close()
+            for filename in files:
+                hdulist = filename[0]
+                with io.BytesIO() as fits_binary_data:
+                    hdulist.writeto(fits_binary_data, checksum=True, overwrite=True)
+                    fits_binary_data.seek(0)
+                    zip_file.writestr(os.path.join(folder_path, f"{filename[4]}_order{filename[3]}.fits"), fits_binary_data.getvalue())
+                hdulist.close()
+    print(time.time()-inicio)
     return temp_zip_path
 
 def worker(queue, dataframe, chunk_index):
@@ -70,43 +74,16 @@ def worker(queue, dataframe, chunk_index):
     queue.put(zip_path)  # Enqueue the path to the result queue
 
 def combine_zip_files(output_zip, temp_zip_paths):
-    """Concatenate multiple ZIP files into a single ZIP file."""
-    with open(output_zip, 'wb') as outfile:
-        for zip_path in temp_zip_paths:
-            if zip_path and os.path.exists(zip_path):
-                with open(zip_path, 'rb') as infile:
-                    shutil.copyfileobj(infile, outfile)
+    """Combine multiple ZIP files into a single ZIP file."""
+    with zipfile.ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as output_zip_file:
+        for temp_zip_path in temp_zip_paths:
+            if temp_zip_path and os.path.exists(temp_zip_path):
+                with zipfile.ZipFile(temp_zip_path, 'r') as temp_zip_file:
+                    for item in temp_zip_file.infolist():
+                        output_zip_file.writestr(item, temp_zip_file.read(item.filename))
             else:
-                print(f"Warning: {zip_path} does not exist or is not valid.")
+                print(f"Warning: {temp_zip_path} does not exist or is not valid.")
     
-def get_files_memory(dataframe, zip_path, lock):
-    """Worker function to process chunks of the dataframe and write to a shared ZIP file."""
-    with lock:
-        with zipfile.ZipFile(zip_path, 'a', zipfile.ZIP_DEFLATED) as zip_file:
-            for idx, oid in dataframe.iterrows():
-                B = {}
-                A = []
-                ra, dec, oid_str = oid['ra'], oid['dec'], str(oid['oid'])
-                
-                for k in range(5):
-                    # Replace this with your actual method to get FITS cutout
-                    fits_cutout = tesis_opt.fits_cutout(ra, dec, 11 - k, 30)
-                    A.append([fits_cutout, ra, dec, 11 - k, oid_str])
-                
-                B[oid_str] = A
-                
-                for folder, files in B.items():
-                    folder_path = os.path.join('multiresolution_fits_files', folder)
-                    zip_file.writestr(folder_path + '/', '')
-
-                    for filename in files:
-                        hdulist = filename[0]
-                        fits_filename = f"{filename[4]}_order{filename[3]}.fits"
-                        with io.BytesIO() as fits_binary_data:
-                            hdulist.writeto(fits_binary_data, checksum=True, overwrite=True)
-                            fits_binary_data.seek(0)
-                            zip_file.writestr(os.path.join(folder_path, fits_filename), fits_binary_data.getvalue())
-                        hdulist.close()
             
 
 target_header = fits.Header.fromstring("""
@@ -212,31 +189,46 @@ def query_stamp_ra_dec_order_size_get(ra, dec, order, size):  # noqa: E501
 
 def query_objects_ra_dec_get(filename):
     df = pd.read_csv(filename)
+    temp_dir = '/tmp/fits_files'
+    tar_path = '/tmp/fits_archive.tar.gz'
+
+    if os.path.exists(temp_dir):
+        shutil.rmtree(temp_dir)  # Clean up the directory if it exists
+    os.makedirs(temp_dir)
+
+    manager = multiprocessing.Manager()
+    jobs = []
     num_chunks = 10
     chunk_size = len(df) // num_chunks
-    zip_path = "/Users/diegoleon/Downloads/combined.zip"
-    lock = multiprocessing.Lock()
-    jobs = []
-    start_time = time.time()
 
-    # Remove the output zip file if it already exists
-    if os.path.exists(zip_path):
-        os.remove(zip_path)
-
+    inicio = time.time()
     for i in range(num_chunks):
-        start_idx = i * chunk_size
-        end_idx = (i + 1) * chunk_size if i != num_chunks - 1 else len(df)
-        chunk_df = df.iloc[start_idx:end_idx]
-        p = multiprocessing.Process(target=get_files_memory, args=(chunk_df, zip_path, lock))
+        start_index = i * chunk_size
+        end_index = (i + 1) * chunk_size if i < num_chunks - 1 else len(df)
+        p = multiprocessing.Process(target=get_files2, args=(df[start_index:end_index], temp_dir))
         jobs.append(p)
         p.start()
 
     for proc in jobs:
         proc.join()
 
-    print(f"Total processing time: {time.time() - start_time} seconds")
-    return send_file(zip_path, mimetype='application/zip', as_attachment=True, download_name='delight_objects.zip')
-   
+    print(f"Image generation time: {time.time() - inicio} seconds")
+
+    inicio2 = time.time()
+
+    # Crear un archivo TAR a partir del directorio
+    with tarfile.open(tar_path, 'w') as tar:
+        tar.add(temp_dir, arcname=os.path.basename(temp_dir))
+
+    print(f"TAR time: {time.time() - inicio2} seconds")
+        
+    # Servir el archivo TAR
+    response = send_file(tar_path, mimetype='application/x-tar', as_attachment=True, download_name='fits_files.tar.gz')
+
+    shutil.rmtree(temp_dir)
+    os.remove(tar_path)
+
+    return response   
 
 
 def query_objects_ra_dec_post():
@@ -266,14 +258,23 @@ def query_objects_ra_dec_post():
     for proc in jobs:
         proc.join()
 
-    print(f"Tiempo de procesamiento: {time.time() - inicio} segundos")
+    print(f"Image generation time: {time.time() - inicio} seconds")
+
+    inicio2 = time.time()
 
     # Crear un archivo TAR a partir del directorio
-    with tarfile.open(tar_path, 'w:gz') as tar:
+    with tarfile.open(tar_path, 'w') as tar:
         tar.add(temp_dir, arcname=os.path.basename(temp_dir))
+
+    print(f"TAR time: {time.time() - inicio2} seconds")
         
     # Servir el archivo TAR
-    return send_file(tar_path, mimetype='application/x-tar', as_attachment=True, download_name='fits_files.tar.gz')
+    response = send_file(tar_path, mimetype='application/x-tar', as_attachment=True, download_name='fits_files.tar.gz')
+
+    shutil.rmtree(temp_dir)
+    os.remove(tar_path)
+
+    return response
 
 def query_delight_ra_dec_get(ra, dec,host=False):  # noqa: E501
     """Set of 5 multiresolution images
@@ -289,7 +290,7 @@ def query_delight_ra_dec_get(ra, dec,host=False):  # noqa: E501
     """
 
     df = pd.DataFrame({ "oid": ["SN2004aq"], "ra": [ra], "dec": [dec]})
-    dclient = Delight("/Users/diegoleon/Desktop/zz", df.oid.values, df.ra.values, df.dec.values)
+    dclient = Delight("/home/dleon/DF", df.oid.values, df.ra.values, df.dec.values)
     try:
         dclient.download(overwrite=True)
     except(FileNotFoundError):
